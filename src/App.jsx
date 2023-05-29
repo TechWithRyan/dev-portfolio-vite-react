27da8e65b659f28d3bf528c62743d0b86138d802
RyanCornelio <ryan.cornelio@gmail.com>
2023-05-29 19:13:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import SideNav from './Components/SideNav'
+import Main from './Components/Main'
+
+
+
+function App() {
+
+
+
+  return (
+
+
+    <div>
+      <SideNav />
+      <Main />
+    </div>
+
+
+  )
+}
+
+export default App
---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div id='main'>
+            <img className="w-full h-screen object-cover object-left scale-x-[-1]" src="https://cdn.pixabay.com/photo/2018/02/23/04/38/computer-3174729_1280.jpg" alt="photo" />
+            {/*            <iframe className="w-full h-screen object-cover object-left" src="https://cdn.pixabay.com/vimeo/241674181/natverk-12716.mp4?width=1280&hash=21bf5cb6abac1a5de51dd909d3ee66fd7e27ce54" frameborder="0"></iframe> */}
+        </div>
+    )
+}
+
+export default Main
