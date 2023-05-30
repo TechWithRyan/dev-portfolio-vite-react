3c9b38ee446b2c4359d37025a4543cd310e66e16
RyanCornelio <ryan.cornelio@gmail.com>
2023-05-30 11:03:57 +0200

    New bg

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,9 +5,9 @@ import { FaYoutube, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 const Main = () => {
     return (
         <div id='main'>
-            <img className="w-full h-screen object-cover object-left scale-x-[-1]" src="https://cdn.pixabay.com/photo/2018/02/23/04/38/computer-3174729_1280.jpg" alt="photo" />
+            <img className="w-full h-screen object-cover object-left " src="https://cdn.pixabay.com/photo/2015/10/21/08/22/media-998990_1280.jpg" alt="photo" />
             {/*            <iframe className="w-full h-screen object-cover object-left" src="https://cdn.pixabay.com/vimeo/241674181/natverk-12716.mp4?width=1280&hash=21bf5cb6abac1a5de51dd909d3ee66fd7e27ce54" frameborder="0"></iframe> */}
-            <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+            <div className="w-full h-screen absolute top-0 left-0 bg-white/60">
 
                 <div className='max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center'>
                     <h1 className='sm:text-5xl text-4xl font-bold text-gray-800 '>I'm Ryan-Phillips</h1>
