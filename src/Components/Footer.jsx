24a887ade278337c07f3fa827c1851806b5de4cc
RyanCornelio <ryan.cornelio@gmail.com>
2023-06-01 12:05:31 +0200

    Added phone number

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-gray-800 text-gray-200">
             <div className="container mx-auto py-6 px-8">
                 <div className="flex flex-col md:flex-row items-center justify-between">
-                    <p className="text-center md:text-left">TechWithRyan</p>
+                    <p className="text-center md:text-left">TechWithRyan: 0760581267</p>
                     <p className="text-center md:text-right">Address: Hällebergsgatan 22 | 442 54 YTTERBY</p>
                     <p className="text-center md:text-left">Email: ryan.cornelio@gmail.com</p>
                 </div>
